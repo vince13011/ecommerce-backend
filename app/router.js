365a55a24d77f02b0b423f37d56d99977f284d0a
MagomedVoraev <voraevmagomed@gmail.com>
2021-03-03 10:37:13 +0100

    router and test.http file done for ARTICLES

--- app/router.js
+++ app/router.js
@@ -1,15 +1,30 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const router = Router();
 
 //validateur  prenant un schema Joi en argument en cas de requete body ou query  pour route post
 // à passer en argument dans notre route post avant le controller
-const { validateBody,validateQuery} = require('./services/validator');
+const { validateBody, validateQuery } = require('./services/validator');
 //schema joi de contraintes avant insertions sql
 const testSchema = require('./joiSchemas/testSchema');
 
 const articleController = require('./controllers/articleController');
 
+// ARTICLES
+router.get('/articles', articleController.getAll);
+router.get('/articles/:id', articleController.getById);
+router.post('/articles', articleController.create);
+router.patch('/articles/:id', articleController.updateById);
+router.delete('/articles/:id', articleController.deleteById);
 
-router.get('/articles',articleController.getAll)
+
+
+// A FAIRE 
+// A FAIRE 
+// A FAIRE
+// retourne la page HOME - d'accueil
+// router.get('/', mainController.homePage);
+
+
+
 
 module.exports = router;
