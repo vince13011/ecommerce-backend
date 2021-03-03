9716c97b163f16fe5fc29c767ecbeeebec2418d2
MagomedVoraev <voraevmagomed@gmail.com>
2021-03-03 14:26:08 +0100

    router updated

--- app/router.js
+++ app/router.js
@@ -1,16 +1,20 @@
 const { Router } = require('express');
 const router = Router();
 
-//validateur  prenant un schema Joi en argument en cas de requete body ou query  pour route post
+// validateur  prenant un schema Joi en argument en cas de requete body ou query  pour route post
 // à passer en argument dans notre route post avant le controller
 const { validateBody, validateQuery } = require('./services/validator');
-//schema joi de contraintes avant insertions sql
+// schema joi de contraintes avant insertions sql
 const testSchema = require('./joiSchemas/testSchema');
 
+// import controllers
 const articleController = require('./controllers/articleController');
 const categoryController = require('./controllers/categoryController');
 const orderController = require('./controllers/orderController');
 const userController = require('./controllers/userController');
+
+// HOME
+// router.get('/', mainController.homePage);
 
 // ARTICLES
 router.get('/articles', articleController.getAll);
@@ -42,8 +46,6 @@ router.get('/users/:id', userController.getOne);
 // router.patch('/users/:id', userController.updateById);
 // router.delete('/users/:id', userController.deleteById);
 
-// HOME
-// router.get('/', mainController.homePage);
 
 
 
