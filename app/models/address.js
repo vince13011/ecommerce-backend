3236bd2a6a4ace170f62e3198053631e5c14cf54
MagomedVoraev <voraevmagomed@gmail.com>
2021-03-04 10:32:34 +0100

    must add user_id as foreign key in table address + can't update table address + role_id appearing in table address : must be corrected

--- app/models/address.js
+++ app/models/address.js
@@ -73,6 +73,23 @@ class Address {
         this.id = rows[0].id;
     }
 
+    /** 
+  * Fonction non statique car propre à chaque instance
+  * Elle permet de modifier un jeu de société  dans notre base de donnée
+  * this correspond au contexte qui est utilisé
+  * dans notre cas il correspond aux données de notre jeu de société avant modification
+  * @param {json} data - Objet json venant modifier les données existantes
+  */
+    async updateById(data) {
+
+        const { rows } = await db.query(`SELECT * FROM update_address($1,$2);`, [data, this.id]);
+        if (rows[0].id === null) {
+            throw new Error(`l'address avec l'id  ${this.id} n'existe pas `)
+        }
+
+        return new Address(rows[0]);
+    }
+
 
 }
 module.exports = Address;
