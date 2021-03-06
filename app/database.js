e417a377b615f9b06e4259860671090aa04900bc
MagomedVoraev <voraevmagomed@gmail.com>
2021-03-06 21:41:15 +0100

    .env as before but added CORS

--- app/database.js
+++ app/database.js
@@ -5,7 +5,7 @@ const { Pool } = require('pg');
 // par contre, en local, il faut qu'il continue de se connecter avec les variables d'environnement de libpq (PGUSER, PGPASSWORD etc.)
 // mais en fait, Pool se débrouille tout seul comme un grand
 const db = new Pool({
-    connectionString: "postgres://pwokrspjqplmgy:c9faa05c76be24b9cbb3bb866cfda93b3148da836175eb254f0a8c0f62defa63@ec2-52-209-134-160.eu-west-1.compute.amazonaws.com:5432/d8ohpsu04as0bb"
+    connectionString: process.env.DATABASE_URL
 });
 
 // maintenant, on n'a plus un seul connecteur mais un pool de connecteurs
