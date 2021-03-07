1e4e90e18fb4582e1bc6fd1a7d9e97cb07348f85
MagomedVoraev <voraevmagomed@gmail.com>
2021-03-07 18:25:38 +0100

    .env changed and database also

--- app/database.js
+++ app/database.js
@@ -4,12 +4,15 @@ const { Pool } = require('pg');
 // avec Heroku, mon Pool va devoir se connecter à DATABASE_URL
 // par contre, en local, il faut qu'il continue de se connecter avec les variables d'environnement de libpq (PGUSER, PGPASSWORD etc.)
 // mais en fait, Pool se débrouille tout seul comme un grand
-const db = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
+
+const config = { connectionString: process.env.DATABASE_URL };
+if (process.env.NODE_ENV === 'production') {
+    config.ssl = {
         rejectUnauthorized: false
     }
-});
+}
+console.log(config);
+const db = new Pool(config);
 
 // maintenant, on n'a plus un seul connecteur mais un pool de connecteurs
 module.exports = db;
